9b5f4224ae424e2c3484bbdc992992b1d985a9aa
Mohammed Rashid <mohmad.rashid@hotmail.com>
2017-07-30 23:04:23 +0530

    Final Upload Version

---
+++ src/components/ng2autobreadcrumb.component.ts
@@ -0,0 +1,51 @@
+import {Component, OnInit} from '@angular/core';
+import { NavigationStart, Router} from "@angular/router";
+
+@Component({
+  selector: 'ng2-auto-breadcrumb',
+  templateUrl: './ng2autobreadcrumb.component.html',
+  styleUrls: ['/ng2autobreadcrumb.component.scss']
+})
+export class Ng2AutoBreadCrumbComponent implements OnInit{
+  ngOnInit(): void {
+    this.path=this.router.url.replace("#","").split("/");
+    this.path.splice(0,1);
+    this.href=this.path.map(function (value) {
+      return value.charAt(0).toUpperCase() + value.slice(1);
+    });
+  }
+
+
+  public disabled: boolean = false;
+  public status: {isopen: boolean} = {isopen: false};
+
+  public path:Array<string>=[];
+  public href:Array<string>=[];
+  public router:Router;
+  constructor(route: Router){
+    this.router=route;
+
+    route.events.forEach((event) => {
+      if(event instanceof NavigationStart) {
+        this.path=event.url.split("/");
+        this.path.splice(0,1);
+        this.href=this.path.map(function (value) {
+          return value.charAt(0).toUpperCase() + value.slice(1);
+        });
+      }
+      // NavigationEnd
+      // NavigationCancel
+      // NavigationError
+      // RoutesRecognized
+    });
+  }
+
+  route(index){
+    let pathToGo="";
+    for(let i=0;i<=index;i++){
+      pathToGo+="/"+this.path[i];
+    }
+    this.router.navigate([pathToGo]);
+
+  }
+}
---
+++ src/ng2autobreadcrumb.module.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by mohma on 7/30/2017.
+ */
+import { NgModule } from '@angular/core';
+import {Ng2AutoBreadCrumbComponent} from "./components/ng2autobreadcrumb.component";
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [ Ng2AutoBreadCrumbComponent],
+  providers: [ ],
+  exports: [Ng2AutoBreadCrumbComponent]
+})
+export class Ng2AutoBreadCrumb {}
